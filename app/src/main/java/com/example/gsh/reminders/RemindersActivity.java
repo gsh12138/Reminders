294de864ef644aaebf3b6272b214baba5043769f
gaoshang <693668332@qq.com>
2016-11-21 20:58:07 +0800

    Adds new reminder and exit menu options

--- app/src/main/java/com/example/gsh/reminders/RemindersActivity.java
+++ app/src/main/java/com/example/gsh/reminders/RemindersActivity.java
@@ -2,11 +2,27 @@ package com.example.gsh.reminders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class RemindersActivity extends AppCompatActivity {
     ListView mListView;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_new:
+                Log.d(getLocalClassName(),"Create new Reminder");
+                return true;
+            case R.id.action_exit:
+                finish();
+                return true;
+            default:return true;
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +35,10 @@ public class RemindersActivity extends AppCompatActivity {
                                                                       new String[]{"first record","second record","third record"});
 
         mListView.setAdapter(mArrayAdapter);
+
+
+
+
+
     }
 }
